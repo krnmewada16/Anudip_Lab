9743caf9307b30dc4e46d51f483fc6a4ff0fc779
krnmewada16 <krnmewada16@gmail.com>
2025-09-09 14:02:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("Hello, this is my first Node.js program!");
+console.log("I am excited to learn more about Node.js!");
+
+let message1 = "Hello, this is my first Node.js program!";
+let message2 = "I am excited to learn more about Node.js!";
+
+let combinedMessage = message1 + message2;
+
+console.log(combinedMessage);
+
+//output
+//Hello, this is my first Node.js program!
+//I am excited to learn more about Node.js!
+//Hello, this is my first Node.js program! I am excited to learn more about Node.js!
